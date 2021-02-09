ba4d743e8554b3b00e62e7240fbaf4547de882d5
Diman <vector_diman@mail.ru>
2021-02-09 19:34:45 +0300

    add: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+
+  if (n < 0)
+      n *= -1;
+
+  let reverseNumber = n % 10;
+  n = Math.floor(n / 10);
+
+  do {
+      reverseNumber = reverseNumber * 10 + n % 10;
+  } while (n = Math.floor(n / 10));
+
+  return reverseNumber;
 }
